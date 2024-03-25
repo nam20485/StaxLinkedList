26eb63234c066be9523081a0af73b76281c186ba
Nathan Miller <nmiller217@gmail.com>
2024-03-25 11:38:30 -0700

    add empty iterator tests class

---
+++ Tests/LinkedListIteratorTests.cpp
@@ -0,0 +1,12 @@
+//#include "gtest/gtest.h"
+#include "pch.h"
+#include "LinkedList.h"
+//#include "StaxAppData.h"
+
+namespace StaxLinkedListTests
+{
+	TEST(LinkedListIteratorTests, TestName) {
+		EXPECT_EQ(1, 1);
+		EXPECT_TRUE(true);
+	}
+}
